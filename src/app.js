8735097ca3576e6c1b28bee66c78686850d00167
thiagocolen <thiago.souzacolen@gmail.com>
2017-12-10 12:12:25 -0200

    react up

---
+++ src/app.js
@@ -0,0 +1,16 @@
+
+
+import React, { Component } from 'react'
+import SearchBar from './searchBar.js'
+
+class App extends Component {
+  render () {
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
+}
+
+export default App
